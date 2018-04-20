84ca7626a326ee41822e9ebc538bb758a6356d37
Alex <alex_greg666@hotmail.com>
2018-04-20 20:32:20 +0100

    First commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void MoveScene(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+}
---
+++ Assets/Scripts/SetResolution.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetResolution : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(540, 960, true);
+	}
+}
